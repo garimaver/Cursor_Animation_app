f6fa8fda35082609af3faec78720982b4dbf3b43
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-04 19:02:43 +0530

    added js

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ document.addEventListener("mousemove", (e) =>{
   let x = e.pageX;
   let y = e.pageY;
 
-  cursor.style.top = y + 'px'
+  cursor.style.top = y + 'px';
+  cursor.style.left = x + 'px';
+  cursor.style.display = 'block';
+
+
+  // stop animation
+  function mouseStopped () {
+    cursor.style.display = 'none';
+    
+  }
 });
